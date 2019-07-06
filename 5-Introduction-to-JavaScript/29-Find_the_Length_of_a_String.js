e6ce09eea839390fbac9b8b8e983ebca37610fe5
Walex111 <owale07@gmail.com>
2019-07-06 10:39:06 -0600

    I added a dot length to the variable on line 13.

---
+++ 5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
@@ -0,0 +1,13 @@
+// I learned to find the lenght of a string
+
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+
+lastNameLength = lastName.length;
