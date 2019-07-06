6e1dbd50faf317af0c95fc195ac6e516da4cdf08
Walex111 <owale07@gmail.com>
2019-07-06 10:48:22 -0600

    I parsed through the variable on line 7 with bracket notation.

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,9 @@
+// Learned about arrays with strings.
+
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+
+var lastName = "Lovelace";
+
+var thirdLetterOfLastName = lastName[2];
