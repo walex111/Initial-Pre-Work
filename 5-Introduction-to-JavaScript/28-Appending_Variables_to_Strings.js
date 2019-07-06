e3370fcb50c5c6e7729b121d4d9bc50e4ea067d4
Walex111 <owale07@gmail.com>
2019-07-06 10:36:11 -0600

    I concatenated the two variables on lines 7 and 8.

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,9 @@
+// I learned to concatenate two variables using the plus equals operator.
+
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+var someAdjective = "so much fun.";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
