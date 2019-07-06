8ee2bd6f02ffe36f4dad06e762f8196a1db37af7
Walex111 <owale07@gmail.com>
2019-07-06 11:07:56 -0600

    Added to the function.

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,8 @@
+// I learned to use functions.
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+    var result = "";
+  result+= "My "+myAdjective+" "+myNoun+" "+myVerb+" very "+myAdverb+".";
+  return result;
+}
+
+wordBlanks("cat", "big", "ran", "quickly");
