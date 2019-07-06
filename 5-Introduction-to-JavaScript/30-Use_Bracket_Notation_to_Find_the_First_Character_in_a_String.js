7e2121a350bedb197bf932f9e19ef53edba0d1cd
Walex111 <owale07@gmail.com>
2019-07-06 10:42:22 -0600

    Used bracket notation on line 11.

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,12 @@
+// I learned about using arrays to parse through a string.
+
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+firstLetterOfLastName = lastName[0];
+ 
