02e8ebeb4817549051c653c04c1c16951b8d55c8
Walex111 <owale07@gmail.com>
2019-07-06 02:33:02 -0600

    I added some strings to the variable on lines 6 and 7.

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,7 @@
+// I learned to use the madlib style of concatination.
+
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+var myName = "Samson";
+var myStr = "My name is " + myName + " and I am well";
