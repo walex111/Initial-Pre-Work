2dc25b883c2d6518e1b286b8f34cfef772289d1c
Walex111 <owale07@gmail.com>
2019-07-06 11:01:54 -0600

    Used a bracket notation on line 9.

---
+++ 5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,9 @@
+// I learned to find characters using bracket notation.
+
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
