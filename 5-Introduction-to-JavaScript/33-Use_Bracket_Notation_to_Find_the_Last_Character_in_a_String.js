f3fa053a6aaeec3d5ad7835a9e389b6b9654ef6e
Walex111 <owale07@gmail.com>
2019-07-06 10:51:21 -0600

    Searched for the last character of the lastname variable on line 8.

---
+++ 5-Introduction-to-JavaScript/33-Use_Bracket_Notation_to_Find_the_Last_Character_in_a_String.js
@@ -0,0 +1,8 @@
+// I learned to find the last character in a variable.
+
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+var lastName = "Lovelace";
+
+var lastLetterOfLastName = lastName[lastName.length - 1];
