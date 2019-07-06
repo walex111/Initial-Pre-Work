e15b878576d626db3bcf5ffa51e6438e345e629c
Walex111 <owale07@gmail.com>
2019-07-06 02:29:00 -0600

    I used concatenating method to merge two strings on lines 6 and 7.

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,7 @@
+// I learned to use the plus equals operator to concatenate strings.
+
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
