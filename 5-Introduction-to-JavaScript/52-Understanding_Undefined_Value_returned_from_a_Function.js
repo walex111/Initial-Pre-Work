f0c275c14005c208f130a724b5f212f603d7a122
Walex111 <owale07@gmail.com>
2019-07-07 08:17:25 -0600

    I an undefined variable function on line 8.

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,11 @@
+// I learned about undefined value returns.
+// Example
+var sum = 0;
+function addThree() {
+ sum = sum + num;
+}
+
+function addFive(){
+sum += 5;
+}
+var returnedValue = addFive();
