b58fad481e3d708ea7c493d5f79c028e208a9fc3
Walex111 <owale07@gmail.com>
2019-07-07 00:22:45 -0600

    Created a new argument function on line 9;

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,13 @@
+// I learned about passing values to the arguments in the array.
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+function functionWithArgs(X, Y){
+  console.log(X + Y);
+
+}
+functionWithArgs(2, 4);
