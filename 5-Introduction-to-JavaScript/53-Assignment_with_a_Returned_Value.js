5edc2ab49a6533f6ef2b6e5cef253b6aff7db9f6
Walex111 <owale07@gmail.com>
2019-07-07 08:20:45 -0600

    I assigned the return value of the processArg to processed.

---
+++ 5-Introduction-to-JavaScript/53-Assignment_with_a_Returned_Value.js
@@ -0,0 +1,18 @@
+// I learned to assign the return value to a variable.
+// Example
+var changed = 0;
+
+function change(num) {
+  return (num + 5) / 3;
+}
+
+changed = change(10);
+
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+processed = processArg(7);
