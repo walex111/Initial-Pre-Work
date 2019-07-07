6371e29edccd65d896541a9e5ef0017c72cec8fb
Walex111 <owale07@gmail.com>
2019-07-07 00:14:33 -0600

    Wrote a function on line 10.

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,13 @@
+// I learned to use a reusable function.
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction(){
+    console.log("Hi World");
+}
+reusableFunction();
