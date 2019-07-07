e519612f8894ff34c474ae95e8862628772fad84
Walex111 <owale07@gmail.com>
2019-07-07 00:43:41 -0600

    Created another function on line 7.

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,12 @@
+// I learned to create a function with returns.
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+function timesFive(num){
+  return num * 5;
+}
+var answer = timesFive(5);
+
+console.log(minusSeven(10));
