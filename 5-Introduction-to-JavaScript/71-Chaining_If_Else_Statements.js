1f8e5374d8c40cbf95ca571b516ed2d6f7fc4dc6
Walex111 <owale07@gmail.com>
2019-07-07 23:52:13 -0600

    I added a chaining else if statement to the function.

---
+++ 5-Introduction-to-JavaScript/71-Chaining_If_Else_Statements.js
@@ -0,0 +1,20 @@
+// I learned about chaining the if else statement
+function testSize(num) {
+  // Only change code below this line
+  if(num < 5){
+    return "Tiny";
+  } else if(num < 10){
+    return "Small";
+  } else if(num < 15) {
+    return "Medium";
+  } else if(num < 20){
+    return "Large";
+  } else if(num >= 20){
+    return "Huge";
+  }
+
+  // Only change code above this line
+}
+
+// Change this value to test
+testSize(7);
