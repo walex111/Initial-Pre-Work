a1eb2327e364fc6b9ec303205db3a1a2f84b0c86
Walex111 <owale07@gmail.com>
2019-07-07 22:32:16 -0600

    I added an if statement to the function on line 11.

---
+++ 5-Introduction-to-JavaScript/56-Use_Conditional_Logic_with_If_Statements.js
@@ -0,0 +1,16 @@
+// I learned to use conditional logic with if statement
+// Example
+function ourTrueOrFalse(isItTrue) {
+  if (isItTrue) {
+    return "Yes, it's true";
+  }
+  return "No, it's false";
+}
+
+// Setup
+function trueOrFalse(wasThatTrue) {
+if(wasThatTrue){
+return "Yes, that was true";
+}
+return "No, that was false";
+}
