df5b67b0d214cd5187a230132abe3e284cbd2206
Walex111 <owale07@gmail.com>
2019-07-08 11:53:39 -0600

    Added a switch statement to the function.

---
+++ 5-Introduction-to-JavaScript/79-Counting_Cards.js
@@ -0,0 +1,26 @@
+// I learned to use the switch statement to count cards
+function cc(card) {
+  // Only change code below this line
+  switch(card){
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+  if (count > 0){
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+  // Only change code above this line
+}
