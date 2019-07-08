7037b10b9f8b761c774514d0a76805a05756f938
Walex111 <owale07@gmail.com>
2019-07-08 14:43:58 -0600

    Changed all var to let.

---
+++ 6-Introduction-to-the-ES6-Challenges/1-Explore-Differences-Between-the-var-and-let-Keywords.js
@@ -0,0 +1,11 @@
+// I learned the difference between car and let.
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
