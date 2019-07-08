061c03c16bd4320a5aa8d1697d6ad05f89b6a628
Walex111 <owale07@gmail.com>
2019-07-08 10:31:21 -0600

    Added a switch statement with default to the function.

--- 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
+++ 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
@@ -1,1 +1,26 @@
-
+// I learned to use switch statement for multiple identical options.
+function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+  switch(val) {
+    case 1:
+    case 2:
+    case 3:
+      return "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      return "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      return "High";
+      break;
+  }
+  // Only change code above this line
+  return answer;
+}
+// Change this value to test
+sequentialSizes(1);
