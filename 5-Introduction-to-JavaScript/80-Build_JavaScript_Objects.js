6b61a66147c94163e5127f9e102df48eeef36d65
Walex111 <owale07@gmail.com>
2019-07-08 11:59:22 -0600

    Added objects to the myDog variable.

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,17 @@
+// Learned to build a javascript object.
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "John",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["many"]
+};
