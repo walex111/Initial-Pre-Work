cd11a9c23c897908f9fc69d1822925fe7d9c1ae9
Walex111 <owale07@gmail.com>
2019-07-07 23:15:54 -0600

    Added some dreater than arg to the if statement.

---
+++ 5-Introduction-to-JavaScript/62-Comparison_with_the_Greater_Than_Operator.js
@@ -0,0 +1,15 @@
+// I learned about the greater than operator.
+function testGreaterThan(val) {
+  if (val > 100) {
+    return "Over 100";
+  }
+
+  if (val > 10) {
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+
+testGreaterThan(10);
