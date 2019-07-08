b2c3a0431e6745bc8d18ae721c91de5187e0d14a
Walex111 <owale07@gmail.com>
2019-07-07 23:20:09 -0600

    Added a less than operator to the if statements.

---
+++ 5-Introduction-to-JavaScript/64-Comparison_with_the_Less_Than_Operator.js
@@ -0,0 +1,14 @@
+// I learned about the less than operator.
+function testLessThan(val) {
+  if (val < 25) {
+    return "Under 25";
+  }
+
+  if (val < 55) {
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+testLessThan(10);
