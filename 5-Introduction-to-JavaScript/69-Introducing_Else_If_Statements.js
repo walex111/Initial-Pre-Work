20fab9ca1a8e7c6bb4e75d567af8e2ba824c3b2d
Walex111 <owale07@gmail.com>
2019-07-07 23:34:31 -0600

    I changed the if statement to an else if operator in the function

---
+++ 5-Introduction-to-JavaScript/69-Introducing_Else_If_Statements.js
@@ -0,0 +1,16 @@
+// I learned to use the else if operator.
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  }
+
+  else if (val < 5) {
+    return "Smaller than 5";
+  }
+  else{
+  return "Between 5 and 10";
+  }
+}
+
+// Change this value to test
+testElseIf(7);
