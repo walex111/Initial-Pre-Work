793731cef2516caa7ad30957949479be9b2dea4d
Walex111 <owale07@gmail.com>
2019-07-08 10:16:17 -0600

    Added switch statement to the function.

---
+++ 5-Introduction-to-JavaScript/73-Selecting_from_Many_Options_with_Switch_Statements.js
@@ -0,0 +1,22 @@
+// I learned to select from different statement with switch.
+function caseInSwitch(val) {
+  switch(val) {
+    case 1:
+      return "alpha";
+      break;
+    case 2:
+      return "beta";
+      break;
+    case 3:
+      return "gamma";
+      break;
+    case 4:
+      return "delta";
+      break;
+  }
+  return answer;
+}
+
+// Change this value to test
+caseInSwitch(1);
+}
