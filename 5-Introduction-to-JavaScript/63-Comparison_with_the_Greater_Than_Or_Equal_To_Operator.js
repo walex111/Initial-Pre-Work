e95e60bf7dc17cb66f3e98e4887678666144a0d4
Walex111 <owale07@gmail.com>
2019-07-07 23:18:13 -0600

    Added a greater or equality operator to the if statement.

---
+++ 5-Introduction-to-JavaScript/63-Comparison_with_the_Greater_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,14 @@
+// I learned about the greater than or equality operator.
+function testGreaterOrEqual(val) {
+  if (val >= 20) {
+    return "20 or Over";
+  }
+
+  if (val >= 10) {
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);
