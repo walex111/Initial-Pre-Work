a7ec60b7cf31260dc12ff3cd976ecdbdc38c2efa
Walex111 <owale07@gmail.com>
2019-07-07 22:52:06 -0600

    I added an equality operator to the function.

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,10 @@
+// I learned about comparison with the equality operator.
+// Setup
+function testEqual(val) {
+  if (val == 12) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testEqual(10);
