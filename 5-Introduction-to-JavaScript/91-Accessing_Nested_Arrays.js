15744a38055e0b924059dc40d5c7ea50cc8f12c0
Walex111 <owale07@gmail.com>
2019-07-08 12:50:00 -0600

    Accesesd the nexted array.

---
+++ 5-Introduction-to-JavaScript/91-Accessing_Nested_Arrays.js
@@ -0,0 +1,24 @@
+// I learned to access nexted arrays
+// Setup
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1];
