4cdc688c7aaf4f2070a0a8e82db86eae7a5dc9de
Walex111 <owale07@gmail.com>
2019-07-07 23:31:58 -0600

    Added a if/else operator to the function.

---
+++ 5-Introduction-to-JavaScript/68-Introducing_Else_Statements.js
@@ -0,0 +1,18 @@
+// I learned about the if/else operator.
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+ else{
+   result = "5 or Smaller";
+ }
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+testElse(4);
