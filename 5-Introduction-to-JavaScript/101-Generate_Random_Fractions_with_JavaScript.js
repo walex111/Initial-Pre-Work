9747eed034c291e327eee64308b4ce0779fbfdba
Walex111 <owale07@gmail.com>
2019-07-08 13:38:49 -0600

    Generated a random number in the function.

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,8 @@
+// I learned to generate a random number.
+function randomFraction() {
+
+  // Only change code below this line.
+  return Math.random();
+
+  // Only change code above this line.
+}
