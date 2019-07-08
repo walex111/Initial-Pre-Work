17df1576db446ff5ad8757c2bd2fb2f6323f211e
Walex111 <owale07@gmail.com>
2019-07-07 23:22:11 -0600

    I added a less than or equality operator to the if statements in the function.

---
+++ 5-Introduction-to-JavaScript/65-Comparison_with_the_Less_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,14 @@
+// I learned about the less than or equality operator.
+function testLessOrEqual(val) {
+  if (val <= 12) {
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val <= 24) {
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+testLessOrEqual(10);
