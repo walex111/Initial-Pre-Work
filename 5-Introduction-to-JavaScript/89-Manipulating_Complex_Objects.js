29b4b00de2c870613fe3edf79a32e1279c749b28
Walex111 <owale07@gmail.com>
2019-07-08 12:35:30 -0600

    Added another object to the array.

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,19 @@
+// I learned about manipulating complex objects
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {"artist": "Wale",
+  "title": "Hey there",
+  "release_year": 2019,
+  "formats": ["youtube", "video"]}
+
+];
