1fce8dee2dd290ff6287ec3c425e739e103fca39
Walex111 <owale07@gmail.com>
2019-07-08 16:20:08 -0600

    I used the ES6 decleration in the function.

---
+++ 6-Introduction-to-the-ES6-Challenges/19-Write-Concise-Declarative-Functions-with-ES6.js
@@ -0,0 +1,12 @@
+// I learned how to write a simpler declerative function wit ES6.
+// change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    "use strict";
+    this.gear = newGear;
+  }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear); 
