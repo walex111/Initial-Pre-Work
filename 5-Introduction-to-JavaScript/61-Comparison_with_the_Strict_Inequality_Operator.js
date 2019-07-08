839167416d1b3d04cde22f4766e4ed98ee5508ec
Walex111 <owale07@gmail.com>
2019-07-07 23:06:05 -0600

    I added a strict inequality operator to the if statement in the function.

---
+++ 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
@@ -0,0 +1,11 @@
+// I learned to use the strict inequality operator
+// Setup
+function testStrictNotEqual(val) {
+
+  if (val!== 17) {
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testStrictNotEqual(10);
