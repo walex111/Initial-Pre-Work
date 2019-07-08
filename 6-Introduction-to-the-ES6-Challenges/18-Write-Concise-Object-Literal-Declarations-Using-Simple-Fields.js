281d8d30151f24ddcc6aa039f4fc8baa3db546f5
Walex111 <owale07@gmail.com>
2019-07-08 16:16:23 -0600

    I used an object literal decleration in arrow function.

---
+++ 6-Introduction-to-the-ES6-Challenges/18-Write-Concise-Object-Literal-Declarations-Using-Simple-Fields.js
@@ -0,0 +1,4 @@
+// i learned to write a concise object literal delearation.
+const createPerson = (name, age, gender) => ({ name, age, gender});
+
+console.log(createPerson("Zodiac Hasbro", 56, "male")); // returns a proper object
