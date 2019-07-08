04fbb8633fccf42ab01ba8ad702699c9a13e6a23
Walex111 <owale07@gmail.com>
2019-07-08 13:12:37 -0600

    I added a for loop.

--- 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
@@ -1,1 +1,15 @@
+// I learned to count backwards with the for loop.
+// Example
+var ourArray = [];
 
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for(var i = 9; i > 0; i -= 2){
+  myArray.push(i);
+}
