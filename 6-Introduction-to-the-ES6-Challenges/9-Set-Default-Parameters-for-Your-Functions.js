abb9ac2798d4cc03bc4feb68c1897f7abf2842c7
Walex111 <owale07@gmail.com>
2019-07-08 15:23:45 -0600

    I added a default parameter to the function.

---
+++ 6-Introduction-to-the-ES6-Challenges/9-Set-Default-Parameters-for-Your-Functions.js
@@ -0,0 +1,9 @@
+// I learned to set default values for the function.
+const increment = (function() {
+  "use strict";
+  return function increment(number, value = 1) {
+    return number + value;
+  };
+})();
+console.log(increment(5, 2)); // returns 7
+console.log(increment(5)); // returns 6
