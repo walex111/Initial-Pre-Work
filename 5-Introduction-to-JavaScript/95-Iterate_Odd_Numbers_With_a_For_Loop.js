5d17525922f78ab2fb4c2f3fb734e0464fa0a0b6
Walex111 <owale07@gmail.com>
2019-07-08 13:09:29 -0600

    I added a for loop.

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,15 @@
+// I learned to use for loop for odd numbers.
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for(var i = 1; i < 10; i += 2){
+  myArray.push(i);
+}
